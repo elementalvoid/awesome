5163b8ee84c682f5b58d683f4841b6830ba6c5bc
Matt Klich <matt.klich@readytalk.com>
2011-04-04 09:39:07 -0600

    updated tags

--- rc.lua
+++ rc.lua
@@ -79,8 +79,12 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4}, s, awful.layout.suit.tile)
 end
+--for s = 1, screen.count() do
+--   -- Each screen has its own tag table.
+--   tags[s] = awful.tag({ "⌨", "☠", "☕", "✍", "☻", "♬" }, s, awful.layout.suit.tile)
+--end
 -- }}}
 
 -- {{{ Menu
