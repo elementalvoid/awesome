98d4274777af67ef030c984e2b7434521b3c3db3
Matt Klich <matt@elementalvoid.com>
2014-02-07 15:08:10 -0700

    enable mousewheel support on volumewidget

--- rc.lua
+++ rc.lua
@@ -162,6 +162,11 @@ volumewidget = widget({
     align = "right"
 })
 
+volumewidget:buttons(awful.util.table.join(
+  awful.button({ }, 4, function() pulseaudio.volumeUp(); volumewidget.text = pulseaudio.volumeInfo() end),
+  awful.button({ }, 5, function() pulseaudio.volumeDown(); volumewidget.text = pulseaudio.volumeInfo() end)
+))
+
 volumewidget.text = pulseaudio.volumeInfo()
 volumetimer = timer({ timeout = 30 })
 volumetimer:add_signal("timeout", function() volumewidget.text = pulseaudio.volumeInfo() end)
